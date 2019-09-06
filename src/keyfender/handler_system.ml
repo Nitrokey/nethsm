248dd3645cc8ee40cbd420d4ee6acd82fa7a3555
linse <sschirme@gmail.com>
2019-09-06 10:59:02 +0200

    /system endpoints are protected by basic auth, accessible for administrator role only

--- src/keyfender/handler_system.ml
+++ src/keyfender/handler_system.ml
@@ -1,4 +1,24 @@
-open Lwt.Infix
+(* Headers *)
+let get_authorization headers = Cohttp.Header.get headers "Authorization"
+
+let get_user headers = match get_authorization headers with
+  | None -> assert false
+  | Some v -> v
+
+let replace_authorization auth headers =
+  Cohttp.Header.replace headers "Authorization" auth
+
+let decode_auth v =
+  match Astring.String.cut ~sep:"Basic " v with
+  | Some ("", b64) ->
+    begin match Nocrypto.Base64.decode (Cstruct.of_string b64) with
+      | None -> Error (`Msg ("invalid base64 encoding " ^ b64))
+      | Some data -> match Astring.String.cut ~sep:":" (Cstruct.to_string data) with
+        | None -> Error (`Msg ("invalid user:pass encoding" ^ Cstruct.to_string data))
+        | Some (user, password) -> Ok (user, password)
+    end
+  | _ -> Error (`Msg ("invalid auth header " ^ v))
+
 
 module Make (Wm : Webmachine.S with type +'a io = 'a Lwt.t) = struct
 
@@ -29,7 +49,28 @@ module Make (Wm : Webmachine.S with type +'a io = 'a Lwt.t) = struct
       | Some "backup" ->  Wm.respond (Cohttp.Code.code_of_status `Not_found) rd
       | Some "restore" -> Wm.respond (Cohttp.Code.code_of_status `Not_found) rd
       | _ -> Wm.respond (Cohttp.Code.code_of_status `Not_found) rd
- 
+
+    method! is_authorized rd =
+      match get_authorization rd.Webmachine.Rd.req_headers with
+      | None -> Wm.continue (`Basic "NitroHSM") rd
+      | Some auth -> 
+        match decode_auth auth with
+        | Ok (username, password) -> 
+          if Hsm.is_authenticated hsm_state ~username ~password
+          then 
+            let rd' = Webmachine.Rd.with_req_headers (replace_authorization username) rd in
+            Wm.continue `Authorized rd'
+          else
+            Wm.continue (`Basic "invalid authorization") rd
+        | Error (`Msg msg) ->
+          Logs.warn (fun m -> m "is_authorized failed with header value %s and message %s" auth msg);
+          Wm.continue (`Basic "invalid authorization") rd
+
+    method! forbidden rd =
+      let user = get_user rd.Webmachine.Rd.req_headers in
+      let granted = Hsm.is_authorized hsm_state user Hsm.Administrator in
+      Wm.continue (not granted) rd
+
     method !process_post rd =
       Wm.continue true rd 
 
--- src/keyfender/hsm.ml
+++ src/keyfender/hsm.ml
@@ -19,21 +19,33 @@ type system_info = {
   hardwareVersion : string ;
 }[@@deriving yojson]
 
+type role = Administrator | Operator | Metrics | Backup
+type user = { name : string ; password : string ; role : role }
+type users = user list
+
 type t = {
   info : info ;
   system_info : system_info ;
   mutable state : state ;
+  users : users ;
 }
 
 let make = { 
   info = { vendor = "Nitrokey UG" ; product = "NitroHSM" ; version = "v1" } ;
   system_info = { firmwareVersion = "1" ; softwareVersion = "0.7rc3" ; hardwareVersion = "2.2.2" } ;
   state = `Unprovisioned ;
+  (* TODO these are dummies *)
+  users = [ { name = "admin" ; password = "test1" ; role = Administrator } ; 
+            { name = "operator" ; password = "test2" ; role = Operator } ] ;
 }
 
 let info t = t.info
 let system_info t = t.system_info
 let state t = t.state
+let is_authenticated t ~username ~password =
+  List.exists (fun u -> u.name = username && u.password = password) t.users
+let is_authorized t username role =
+  List.exists (fun u -> u.name = username && u.role = role) t.users
 
 let provision t ~unlock ~admin time = t.state <- `Operational 
 
