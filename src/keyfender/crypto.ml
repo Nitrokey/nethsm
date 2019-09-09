d32abb1fa33053a9c90fe0c5f8aaa06866699ea1
Hannes Mehnert <hannes@mehnert.org>
2019-09-09 14:56:41 +0200

    basic key derivation, decryption and encryption of domain key

---
+++ src/keyfender/crypto.ml
@@ -0,0 +1,41 @@
+
+let count = 1000
+let key_len = 16l
+
+module K = Pbkdf.Make(Nocrypto.Hash.SHA256)
+
+let unlock_key_of_passphrase ~salt password =
+  (* results in the key data for the individual stores (authentication, key) *)
+  K.pbkdf2
+    ~password:(Cstruct.of_string password)
+    ~salt:(Cstruct.of_string salt)
+    ~count ~dk_len:key_len
+
+module GCM = Nocrypto.Cipher_block.AES.GCM
+
+let dk_adata = Cstruct.of_string "domain key"
+
+let iv_size = 12
+
+let decrypt_domain_key ~unlock_key data =
+  (* data is a single cstruct, representing (nonce/iv, tag, enc data) *)
+  if Cstruct.len data < 16 + iv_size + GCM.block_size then
+    Error (`Msg "data too small")
+  else
+    let iv, data' = Cstruct.split data iv_size in
+    let ctag, data'' = Cstruct.split data' 16 in
+    let { GCM.message ; tag } =
+      GCM.decrypt ~key:(GCM.of_secret unlock_key) ~iv ~adata:dk_adata data''
+    in
+    if Cstruct.equal tag ctag then
+      Ok message
+    else
+      Error (`Msg "not authenticated")
+
+let encrypt_domain_key rng ~unlock_key data =
+  let iv = rng iv_size in
+  let { GCM.message ; tag } =
+    GCM.encrypt ~key:(GCM.of_secret unlock_key) ~iv ~adata:dk_adata data
+  in
+  Cstruct.concat [ iv ; tag ; message ]
+
---
+++ src/keyfender/test/test_crypto.ml
@@ -0,0 +1,63 @@
+open OUnit
+
+let domain_key = Mirage_random_test.generate 32
+
+let basic_enc_dec_ok () =
+  let unlock_key = Mirage_random_test.generate 16 in
+  let encrypted =
+    Keyfender.Crypto.encrypt_domain_key
+      Mirage_random_test.generate ~unlock_key domain_key
+  in
+  "decrypting an encrypted domain key works" @?
+  match Keyfender.Crypto.decrypt_domain_key ~unlock_key encrypted with
+  | Ok data -> Cstruct.equal data domain_key
+  | _ -> false
+
+let basic_enc_dec_fail_not_authenticated () =
+  let unlock_key = Mirage_random_test.generate 16 in
+  let encrypted =
+    Keyfender.Crypto.encrypt_domain_key
+      Mirage_random_test.generate ~unlock_key domain_key
+  in
+  "decrypting an encrypted domain key fails (wrong unlock key)" @?
+  match Keyfender.Crypto.decrypt_domain_key ~unlock_key:(Mirage_random_test.generate 16) encrypted with
+  | Ok _ -> false
+  | Error _ -> true (* expecting the not authenticated message *)
+
+let basic_enc_dec_fail_too_small () =
+  "decrypting an encrypted domain key fails (bad encrypted)" @?
+  match Keyfender.Crypto.decrypt_domain_key ~unlock_key:(Mirage_random_test.generate 16) Cstruct.empty with
+  | Ok _ -> false
+  | Error _ -> true (* expecting the data too small message *)
+
+let unlock_key passphrase =
+  let salt = "ABCDEF" in
+  Keyfender.Crypto.unlock_key_of_passphrase ~salt passphrase
+
+let kdf () =
+  let passphrase = "einszweidreivier" in
+  let unlock = unlock_key passphrase in
+  "run KDF twice results in the same unlock key" @?
+  Cstruct.equal unlock (unlock_key passphrase)
+
+(* translate from ounit into boolean *)
+let rec ounit_success =
+  function
+    | [] -> true
+    | RSuccess _::t
+    | RSkip _::t ->
+        ounit_success t
+    | RFailure _::_
+    | RError _::_
+    | RTodo _::_ ->
+        false
+
+let () =
+  let tests = [
+    "basic encryption and decryption" >:: basic_enc_dec_ok;
+    "basic encryption and decryption fail (not authenticated)" >:: basic_enc_dec_fail_not_authenticated;
+    "basic encryption and decryption fail (data too small)" >:: basic_enc_dec_fail_too_small;
+    "KDF" >:: kdf;
+  ] in
+  let suite = "test crypto" >::: tests in
+  if not (ounit_success (run_test_tt suite)) then exit 1
