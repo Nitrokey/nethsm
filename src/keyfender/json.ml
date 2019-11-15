d7a6d65c07f2a9c667223e8a9181227f9bb92b60
Hannes Mehnert <hannes@mehnert.org>
2019-11-15 11:16:44 +0100

    M-x whitespace-cleanup

--- src/keyfender/json.ml
+++ src/keyfender/json.ml
@@ -1,6 +1,6 @@
 (* request data *)
 
-type subject_req = { 
+type subject_req = {
     countryName : string ;
     stateOrProvinceName : string ;
     localityName : string ;
@@ -14,20 +14,20 @@ let nonempty ~name s =
   if String.length s == 0
   then Error (Printf.sprintf "JSON field %s is empty." name)
   else Ok ()
- 
+
 let decode parse data =
   let open Rresult.R.Infix in
   (try Ok (Yojson.Safe.from_string data)
    with Yojson.Json_error msg -> Error (Printf.sprintf "Invalid JSON: %s." msg)) >>= fun json ->
-  Rresult.R.reword_error (fun m -> Printf.sprintf "Invalid data for JSON schema: %s." m) 
+  Rresult.R.reword_error (fun m -> Printf.sprintf "Invalid data for JSON schema: %s." m)
     @@ parse json
 
-let decode_time s = 
+let decode_time s =
   let open Rresult.R.Infix in
   (* since ~sub:true is _not_ passed to of_rfc3339,
      no trailing bytes (third return value will be String.length b.time) *)
-  Rresult.R.reword_error (function `RFC3339 ((start, stop), e) -> 
-    Fmt.strf "Failed to decode timestamp: %a at position %d to %d." Ptime.pp_rfc3339_error e start stop) 
+  Rresult.R.reword_error (function `RFC3339 ((start, stop), e) ->
+    Fmt.strf "Failed to decode timestamp: %a at position %d to %d." Ptime.pp_rfc3339_error e start stop)
     (Ptime.of_rfc3339 s) >>= fun (time, off, _) ->
   (* according to spec, we accept only UTC timestamps! *)
   (match off with None | Some 0 -> Ok () | _ -> Error "Error while parsing timestamp. Offset must be 0.") >>| fun () ->
@@ -35,9 +35,9 @@ let decode_time s =
 
  (* TODO json object or string? *)
 type passphrase_req = { passphrase : string } [@@deriving yojson]
-  
+
 let decode_passphrase json =
   let open Rresult.R.Infix in
   decode passphrase_req_of_yojson json >>= fun passphrase ->
   nonempty ~name:"passphrase" passphrase.passphrase >>| fun () ->
-  passphrase.passphrase 
+  passphrase.passphrase
