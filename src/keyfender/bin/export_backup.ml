7fc78b6017662df73163411f32995104d4aa36c1
linse <sschirme@gmail.com>
2020-02-07 17:23:02 +0100

    export_backup.exe is a tool to export a backup image from binary format to json.

---
+++ src/keyfender/bin/export_backup.ml
@@ -0,0 +1,165 @@
+open Keyfender
+
+let read_file filename =
+  let filesize = (Unix.stat filename).Unix.st_size in
+  let fd = Unix.openfile filename [Unix.O_RDONLY] 0 in
+  let buf = Bytes.create filesize in
+  let rec read off =
+    if off = filesize
+    then ()
+    else
+      let bytes_read = Unix.read fd buf off (filesize - off) in
+      read (bytes_read + off)
+  in
+  read 0;
+  Unix.close fd;
+  Bytes.to_string buf
+
+(*
+let read_file_chunked filename hash prepend_length output =
+  let filesize = (Unix.stat filename).Unix.st_size in
+  let hash' =
+    if prepend_length
+    then output hash @@ Bytes.of_string @@ write_len filesize
+    else hash
+  in
+  let chunksize = 4096 in
+  let fd = Unix.openfile filename [Unix.O_RDONLY] 0 in
+  let buf = Bytes.create chunksize in
+  let rec read hash off =
+    if off = filesize
+    then hash
+    else
+      let bytes_read = Unix.read fd buf 0 (min chunksize (filesize - off)) in
+      let hash' = output hash (Bytes.sub buf 0 bytes_read) in
+      read hash' (bytes_read + off)
+  in
+  let hash'' = read hash' 0 in
+  Unix.close fd;
+  hash''
+*)
+
+(*
+let read_n stream n =
+  let rec read prefix =
+    let open Lwt.Infix in
+    Lwt_stream.get stream >>= function
+    | None -> Lwt.return @@ Error (Bad_request, "Malformed update")
+    | Some data ->
+      let str = prefix ^ data in
+      if String.length str >= n
+      then
+        let data, rest = Astring.String.span ~min:n ~max:n str in
+        Lwt.return @@ Ok (data, put_back stream rest)
+      else read str
+  in
+  read ""
+*)
+
+let decode_length data =
+  let data' = Cstruct.of_string data in
+  let byte = Cstruct.get_uint8 data' 0 in
+  let len = Cstruct.BE.get_uint16 data' 1 in
+  byte lsl 16 + len
+
+(*
+let get_length stream =
+  let open Lwt_result.Infix in
+  read_n stream 3 >|= fun (data, stream') ->
+  let length = decode_length data in
+  (length, stream')
+
+let get_data (l, s) = read_n s l
+*)
+
+let get_field s =
+  let len = decode_length s in
+  let offset = 3 + len in
+  String.sub s 3 len, String.sub s offset (String.length s - offset)
+
+let version = "0"
+  
+let err_to_msg = function
+  | Error e -> Error (`Msg e)
+  | Ok a -> Ok a
+
+let export passphrase backup_image_filename output =
+  err_to_msg @@
+  let backup_data = read_file backup_image_filename in
+  let salt, backup_data' = get_field backup_data in
+  let backup_key =
+    Crypto.key_of_passphrase ~salt:(Cstruct.of_string salt) passphrase
+  in
+  let key = Crypto.GCM.of_secret backup_key in
+  let encrypted_version, backup_data'' = get_field backup_data' in
+  let adata = Cstruct.of_string "backup-version" in
+  match Crypto.decrypt ~key ~adata (Cstruct.of_string encrypted_version) with
+  | Error `Insufficient_data ->
+    Error "Could not decrypt backup version. Backup incomplete, try another one."
+  | Error `Not_authenticated ->
+    Error "Could not decrypt backup version, authentication failed. Is the passphrase correct?"
+  | Ok backup_version ->
+    if String.equal version (Cstruct.to_string backup_version) then
+      begin
+        let rec next acc backup_data =
+          if backup_data = "" then Ok acc else 
+          let item, rest = get_field backup_data in
+          let adata = Cstruct.of_string "backup" in (* TODO use backup2 *)
+          match Crypto.decrypt ~key ~adata (Cstruct.of_string item) with
+          | Error `Insufficient_data ->
+            Error "Could not decrypt stored key-value pair. Backup is corrupted?"
+          | Error `Not_authenticated ->
+            Error "Could not decrypt stored key-value pair. Authentication failed midway. Backup is corrupted?"
+          | Ok key_value_pair ->
+          let key, value = get_field (Cstruct.to_string key_value_pair) in
+          next ((key, value) :: acc) rest 
+        in
+        match next [] backup_data'' with
+        | Error e -> Error e
+        | Ok kvs -> 
+          let fd = match output with
+           | None -> Unix.stdout
+           | Some filename ->
+             if Sys.file_exists filename
+             then invalid_arg "Output file already exists"
+             else Unix.openfile filename [Unix.O_WRONLY ; Unix.O_CREAT] 0o400 
+          in
+          let channel = Unix.out_channel_of_descr fd in
+          let base64 s = Cstruct.to_string @@ Nocrypto.Base64.encode @@ Cstruct.of_string s in
+          let json = `Assoc (List.map (fun (k, v) -> k, `String (base64 v)) kvs) in
+          Yojson.Basic.pretty_to_channel channel json;
+          Unix.close fd;
+          Ok ()
+      end
+    else
+      let msg =
+        Printf.sprintf
+          "Backup version mismatch, provided backup image version is %s, tool expects %s"
+          (Cstruct.to_string backup_version) version
+      in
+      Error msg
+ 
+
+open Cmdliner
+
+let key =
+  let doc = "backup-passphrase" in
+  Arg.(required & pos 0 (some string) None & info [] ~doc ~docv:"KEY")
+
+let backup_image =
+  let doc = "backup image filename" in
+  Arg.(required & pos 1 (some string) None & info [] ~doc ~docv:"IMAGE")
+
+let output =
+  let doc = "output filename" in
+  Arg.(value & opt (some string) None & info [ "output" ] ~doc)
+
+let command =
+  let doc = "Export a NitroHSM backup image to json" in
+  let man = [ `S "BUGS"; `P "Submit bugs";] in
+  Term.(term_result (const export $ key $ backup_image $ output)),
+  Term.info "export_backup" ~version:"%%VERSION_NUM%%" ~doc ~man
+
+let () =
+  Nocrypto_entropy_unix.initialize ();
+  match Term.eval command with `Ok () -> exit 0 | _ -> exit 1
---
+++ src/keyfender/test/test_export_backup.ml
@@ -0,0 +1,41 @@
+let output = {|{
+  "/key/.version": "CVn4OTHFgCGiM5BEaitk6Q0gxjhD+OvTFdpaaAU=",
+  "/domain-key/0":
+    "dfdIH9N6KZSRyzgH9/m6YKrKnPJjc6XNTCN9JiARNijApM/vwqolM5G512L+PVM8N8TcPsd2AH8Bl6u5kSTLaKr0iQD5p+KE1ve/HrG8eTtzG3VfQfHxmbEgWyw=",
+  "/config/version": "MA==",
+  "/config/unlock-salt": "wJ1WGdcsKOwR4tFFzXafXA==",
+  "/config/time-offset": "LTQwMTkyMDQ1",
+  "/config/private-key":
+    "LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tCk1JSUNkd0lCQURBTkJna3Foa2lHOXcwQkFRRUZBQVNDQW1Fd2dnSmRBZ0VBQW9HQkFMWEl0dnJUSy9PZDgxU08KY2lHTFJMOFhxa2IrM0x0S1BPKzdobFpHM2J3RlBuSXNuaHRmQkZuMWswWDM2NzRHTjNHY3pFYUdUaGlhY0dOQgphZU05YlpSNEthWCsvNWhhTnUxbkkwakFaVFQ2eTZUSEpacmhxRFlqR2tEZjlIb2hBSXl5LzdEZjN6NjY1anpVCkYzcno3TTZiejd5RnVjQ3poK2FGMmQrSEhaWDNBZ01CQUFFQ2dZRUFsUExBVTJjaVZUd3Z2REgzZkZyRDJwd0cKYnFPM3MxS1IrWkM2OHdpZitNYUhNMEF4MEpXYldDYXJKS0tPckNZbDJkYXpYbm4vbFpNOTBXWXczL3hlUXUzeQp0NDg1V0EyTjdtYXAxMUozSFViMElyTi84bGE3ampMQzlrbVpmM2dQREEwR0ZGTXRnSTY2dE5HS3JEM1NFQUtWCnJhQzd3d0Nsc0ZlUm1ES3dzV2tDUVFEakQ1QjN4aHY1TTc2VnFocDdjeXVNSG9nbm1oY09WWDdEL3JjWU51ZGwKQmt4bERVWUQ2cUliRTIwbXBBTkZVRDJpZVdjTHA4UTRULzJob1FJREtPVERBa0VBelBQaGxYY0pDemNVZi9YdQpqKzduN0N4OXh5NmVyVENiUXcwWkI5UWJvblNsaldkdkt1bWYyY1lReXdqZkUvZlBDVnRkL25zRlh0TmtGbEozCmhhMW12UUpBYVU2ZnB4MG5maVZrZjg4N3B3T2NMOGs5Um5pdHRzVXFITVoycm5GUVVrZGdpUUZsOW8wTWo5Z3IKcUY4cThTdXMyanpKOE5ybkkyTGQ4K2RUb3BtRnR3SkFid2Z3UGJpdS9aUnBWcWsxQkdTNDRBbDZXbDIzT0x3TQpZTmJIcUtZUUVvSis3WXdMZllQelJWYzM0Y1o4azZTOFZLVmExbkp2Qm1BZTZMamtaMWI1UVFKQkFMOWFBTVV4CnVGbTFsc2RzVnFXY0VoZ0R3Nzdoak9MWE9wc1F5QWowYWVsMFd1ZDBDYkpCWGd5dTdzdk5IekRNeUNNd3lZWksKUnprYUVaTzFtSWxjTFowPQotLS0tLUVORCBQUklWQVRFIEtFWS0tLS0tCg==",
+  "/config/certificate":
+    "AAABrjCCAaowggEToAMCAQICCQCHftzZMvxvHjANBgkqhkiG9w0BAQsFADAUMRIwEAYDVQQDDAlrZXlmZW5kZXIwJBcNNzAwMTAxMDAwMDAwWhgTOTk5OTEyMzEyMzU5NTkuOTk5WjAUMRIwEAYDVQQDDAlrZXlmZW5kZXIwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBALXItvrTK/Od81SOciGLRL8Xqkb+3LtKPO+7hlZG3bwFPnIsnhtfBFn1k0X3674GN3GczEaGThiacGNBaeM9bZR4KaX+/5haNu1nI0jAZTT6y6THJZrhqDYjGkDf9HohAIyy/7Df3z665jzUF3rz7M6bz7yFucCzh+aF2d+HHZX3AgMBAAEwDQYJKoZIhvcNAQELBQADgYEApsJ5UZnp9L50E8O9WJh5rNMEWRiyEXvf0dZANorCIriIz+QuSKNLOcyxxqTYl9dBu6+Zf9/TpOTv8anf4PszPPY/fFPtQvlBqN7lEXi/GCXfx8alBMPT+CtMWWkaLh2uqnyl3Et4PvcQ9jQopNNGcAu2uIv7fx1XQ7fUPwCdGnE=",
+  "/config/backup-salt": "WnFUpfK/BFP6sBCyMxUQzg==",
+  "/config/backup-key": "aCckwOHBwVZdoubMisZkoegQe3LIJd3f6OzzzyeWbs4=",
+  "/authentication/ea2fcb83c49f854535ac":
+    "xqH/XNfGj+4hY2fEa+KKSk3Gz+tr/nKPvhc4PJ5fvDyI7ry36vdRgPihcC47i1A6SDNeLB+Z4GCqCAe7CZfSxQ91xgMfONKxE4WFE5FQJi7mAgyyuqYNdi9DvFDSv2FBIY1dsaF2uOE8itpXLAkhStdeAWXQCsqa1+K1WEvKLBRMAjyC9bMKo1GZ",
+  "/authentication/admin":
+    "MTkd0Q/4fsNyP9wOz/+ZOUbFrgZkp+XT/XvchXKtWIiLvZnTxc99jmLRyld8kpdaecng1xZL8qqqVvlRIxBK2MRv6r9YCWbaBwOgKJ3ofLzAMcCmQrm+BtbSoUqoAOKQ2VRD4y42l0ou1Wv6Qi4oFkUDL5cQe1GTU9Em/yYZFIrFW7hKdu5bYaZkz3fN8kzhGNJ90sd1weWkHXuC2TeAnf6nJ9A=",
+  "/authentication/.version": "1Qc1dIjSZrWgjfemqP+6LRBnl3b92HDAFij2+Uc="
+}|}
+
+let readfile filename =
+  let fd = Unix.openfile filename [Unix.O_RDONLY] 0 in
+  let filesize = (Unix.stat filename).Unix.st_size in
+  let buf = Bytes.create filesize in
+  let rec read off =
+    if off = filesize
+    then ()
+    else
+      let bytes_read = Unix.read fd buf off (filesize - off) in
+      read (bytes_read + off)
+  in
+  read 0;
+  Unix.close fd;
+  Bytes.to_string buf
+ 
+let () =
+  let returncode = Sys.command "../bin/export_backup.exe backup_passphrase my_backup.bin --output=my_backup.json" in
+  assert (returncode = 0);
+  let body = readfile "my_backup.json" in
+  assert (String.equal output body)
+   
