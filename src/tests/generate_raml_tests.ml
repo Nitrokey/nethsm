85d8412534889fad88c04f6e73e749c2ae9a42b7
linse <sschirme@gmail.com>
2020-06-08 15:51:55 +0200

    Recurse over endpoint paths in YAML.

--- src/tests/generate_raml_tests.ml
+++ src/tests/generate_raml_tests.ml
@@ -12,10 +12,56 @@ type raml = { header ..;
 
 *)
 
-let example = CCIO.with_in "../../docs/nitrohsm-api.raml" CCIO.read_all
+let host = "localhost"
+let port = "2000"
+let raml = "../../docs/nitrohsm-api.raml"
+let allowed_methods = ["get" ; "put" ; "post"]
+
+let get_endpoints meta = 
+  Ezjsonm.get_dict meta |> List.partition (fun (key, _v) -> CCString.prefix ~pre:"/" key)
+
+let get_meth meth meta = (* e.g. met is "get", "put", "post" *)
+  Ezjsonm.get_dict meta |> List.partition (fun (key, _v) -> key = meth)
+
+(* TODO for each mediatype, set a header, get the example request *)
+let make_req_data req = function
+  | "get" -> ""
+  | "post" -> Ezjsonm.(value_to_string (find req ["body"]))
+  | "put" -> Ezjsonm.(value_to_string (find req ["body"]))
+  | m -> Printf.printf "method %s not allowed" m; ""
+
+let rec print_path (path, meta) =
+  let (endpoints, _metadata) = get_endpoints meta in
+  if endpoints <> [] 
+  then List.iter (fun (subpath, m) -> print_path (path ^ subpath, m)) endpoints
+  else begin 
+    Printf.printf "Path is %s\n" path;
+    Printf.printf "As YAML:\n%s\n=======\n" (Yaml.to_string_exn meta);
+    let methods = Ezjsonm.get_dict meta in
+    let p (meth, req) =
+      if List.mem meth allowed_methods (* skips descriptions *)
+      then begin 
+        let cmd = Printf.sprintf "curl http://%s:%s%s -X %s" host port path (String.uppercase_ascii meth) in
+        Printf.printf "%s %s \n\n" cmd (make_req_data req meth);
+      end
+    in
+    List.iter p methods
+  end
+
+let example = CCIO.with_in raml CCIO.read_all
   |> Yaml.of_string
   |> Stdlib.Result.get_ok
-let () = Sexplib.Sexp.pp_hum Format.std_formatter @@ Yaml.sexp_of_value example
-let (endpoints, metadata) = Ezjsonm.get_dict example |> List.partition (fun (key, _v) -> CCString.prefix ~pre:"/" key)
+
+(*let () = Sexplib.Sexp.pp_hum Format.std_formatter @@ Yaml.sexp_of_value example*)
+let (endpoints, metadata) = get_endpoints example 
 
 let types = List.assoc "types" metadata
+
+
+let () = List.iter print_path [List.nth endpoints 10]
+
+
+(* all paths, start from empty root *)
+(*
+let () = print_path ("", example)
+*)
