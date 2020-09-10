3fae4295d930dc3348a08619e12d085df47fc0d1
linse <sschirme@gmail.com>
2020-09-10 15:20:43 +0200

    Tests: Don't do extra setup / teardown for negative tests. Make test loop stop again on a non-empty diff.

--- src/tests/run_generated_tests.sh
+++ src/tests/run_generated_tests.sh
@@ -7,6 +7,31 @@ test_one () {
     PID=$!
     sleep 2
     ./setup.sh || (kill $PID ; exit 3)
+
+    # if exists, run ./wrong_json_cmd.sh and see if we get a 400
+    if [ -e wrong_json_cmd.sh ]; then
+      ./wrong_json_cmd.sh || (kill $PID ; exit 4)
+      diff -w -u <(head -n 1 wrong_json_headers.out | cut -f 2 -d ' ') <(echo "400")
+    fi
+
+    # if exists, run ./wrong_key_cmd.sh and see if we get a 404
+    if [ -e wrong_key_cmd.sh ]; then
+      ./wrong_key_cmd.sh || (kill $PID ; exit 4)
+      diff -w -u <(head -n 1 wrong_key_headers.out | cut -f 2 -d ' ') <(echo "404")
+    fi
+
+    # if exists, run ./wrong_user_cmd.sh and see if we get a 404
+    if [ -e wrong_user_cmd.sh ]; then
+      ./wrong_user_cmd.sh || (kill $PID ; exit 4)
+      diff -w -u <(head -n 1 wrong_user_headers.out | cut -f 2 -d ' ') <(echo "404")
+    fi
+
+    # if exists, run ./wrong_auth_cmd.sh and see if we get a 404
+    if [ -e wrong_auth_cmd.sh ]; then
+      ./wrong_auth_cmd.sh || (kill $PID ; exit 4)
+      diff -w -u <(head -n 1 wrong_auth_headers.out | cut -f 2 -d ' ') <(echo "403")
+    fi
+
     ./command.sh || (kill $PID ; exit 4)
     ./shutdown.sh || (kill $PID ; exit 5)
 
@@ -15,55 +40,8 @@ test_one () {
       diff -w -u body.out body.expected
     fi
 
-    # if exists, run ./wrong_json_cmd.sh and see if we get a 400
-    if [ -e wrong_json_cmd.sh ]; then
-      "../../../keyfender/_build/default/test/test_server.exe" &
-      PID=$!
-      sleep 2
-      ./setup.sh || (kill $PID ; exit 3)
-      ./wrong_json_cmd.sh || (kill $PID ; exit 4)
-      ./shutdown.sh || (kill $PID ; exit 5)
-
-      diff -w -u <(grep "^HTTP" wrong_json_headers.out) <(echo "HTTP/1.1 400 Bad Request")
-    fi
-
-    # if exists, run ./wrong_key_cmd.sh and see if we get a 404
-    if [ -e wrong_key_cmd.sh ]; then
-      "../../../keyfender/_build/default/test/test_server.exe" &
-      PID=$!
-      sleep 2
-      ./setup.sh || (kill $PID ; exit 3)
-      ./wrong_key_cmd.sh || (kill $PID ; exit 4)
-      ./shutdown.sh || (kill $PID ; exit 5)
-
-      diff -w -u <(grep "^HTTP" wrong_key_headers.out) <(echo "HTTP/1.1 404 Not Found")
-    fi
-
-    # if exists, run ./wrong_user_cmd.sh and see if we get a 404
-    if [ -e wrong_user_cmd.sh ]; then
-      "../../../keyfender/_build/default/test/test_server.exe" &
-      PID=$!
-      sleep 2
-      ./setup.sh || (kill $PID ; exit 3)
-      ./wrong_user_cmd.sh || (kill $PID ; exit 4)
-      ./shutdown.sh || (kill $PID ; exit 5)
-
-      diff -w -u <(grep "^HTTP" wrong_user_headers.out) <(echo "HTTP/1.1 404 Not Found")
-    fi
-
-    # if exists, run ./wrong_auth_cmd.sh and see if we get a 404
-    if [ -e wrong_auth_cmd.sh ]; then
-      "../../../keyfender/_build/default/test/test_server.exe" &
-      PID=$!
-      sleep 2
-      ./setup.sh || (kill $PID ; exit 3)
-      ./wrong_auth_cmd.sh || (kill $PID ; exit 4)
-      ./shutdown.sh || (kill $PID ; exit 5)
-
-      diff -w -u <(grep "^HTTP" wrong_auth_headers.out) <(echo "HTTP/1.1 403 Forbidden")
-    fi
 }
 
 for test_dir in $(ls generated/); do
-    (cd generated/${test_dir}; test_one) || exit 1
+    (cd generated/${test_dir}; test_one)
 done
