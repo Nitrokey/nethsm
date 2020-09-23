4bf07bb0ef3cd9e3da7985eb3abc17057e0d2d06
Hannes Mehnert <hannes@mehnert.org>
2020-09-23 19:30:33 +0200

    metrics_gnuplot.sh: use a timeout of 1 second

--- src/tests/metrics_gnuplot.sh
+++ src/tests/metrics_gnuplot.sh
@@ -8,7 +8,7 @@ echo "# statistics gathered in tab-separated columns:" >> $tmp
 echo "# 'uptime' 'gc major' 'malloc allocated' 'gc major collections' 'gc compactions' 'http responses' 'kv write' 'http response time' " > $tmp
 
 while true; do
-    curl -s --insecure https://metrics:MetricsMetrics@192.168.1.1/api/v1/metrics |
+    curl -m 1 -s --insecure https://metrics:MetricsMetrics@192.168.1.1/api/v1/metrics |
         jq --raw-output '[ .uptime, ."gc major bytes", ."total allocated space", ."gc major collections", ."gc compactions", ."http response total", ."kv write", ."http response time" ] | @csv' | tr -d '"' | tr ',' '\t' >> $tmp;
     sleep 1
 done
