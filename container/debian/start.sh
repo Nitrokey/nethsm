d228a12dab3128c4d8c40adbd94d86a800e3f9c6
Sven Anderson <sven@anderson.de>
2020-11-14 15:21:24 +0100

    container: improve debian image

--- container/debian/start.sh
+++ container/debian/start.sh
@@ -10,21 +10,23 @@ fi
 
 /setup-net-dev.sh
 
+iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 8443 \
+  -j DNAT --to-destination 192.168.1.1:443
+iptables -t nat -A POSTROUTING -o tap200 -j SNAT --to-source 192.168.1.100
+
 if [ ! -d /data/keyfender-data.git ] ; then
   mkdir /data/keyfender-data.git
   git init --bare /data/keyfender-data.git
 fi
 
-simpleproxy -L 8443 -R 192.168.1.1:443 &
-
-git-daemon \
+git daemon \
     --listen=169.254.169.2 \
     --base-path=/data \
     --export-all \
     --enable=receive-pack \
     &
 
-solo5-hvt \
+/solo5-hvt \
     --net:external=tap200 \
     --net:internal=tap201 \
     /keyfender.hvt
