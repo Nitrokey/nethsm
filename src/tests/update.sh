a0c277b533ed5ed91dbbdb0bf8e58ba0fc0473e3
Markus Meissner <coder@safemailbox.de>
2022-02-21 23:23:39 +0100

    add 'commit-update' call to tests/update.sh script

--- src/tests/update.sh
+++ src/tests/update.sh
@@ -5,6 +5,15 @@ source "$(dirname $0)/common_functions.sh"
 [ -z "$1" ] && echo "usage: $0 obj/update.img.bin" 1>&2 && exit 1
 [ ! -f "$1" ] && echo "no such file: $1" 1>&2 && exit 1
 
+STATE=$(GET /v1/health/state | jq -r '.state')
+
+
+if [ ! $STATE = "Operational" ]; then
+	echo "NetHSM not Operational, but $STATE"
+	exit 1
+fi
+
+
 echo "Updating."
 url="${NITROHSM_URL}/v1/system/update"
 echo "POST ${url}" 1>&2
@@ -12,3 +21,12 @@ curl -k -X POST -H 'Content-Type: application/octet-stream' \
     --user admin:Administrator \
     --data-binary @$1 \
     ${url}
+
+
+url="${NITROHSM_URL}/v1/system/commit-update"
+echo "POST ${url}" 1>&2
+curl -k -X POST \
+    --user admin:Administrator \
+    ${url}
+
+
