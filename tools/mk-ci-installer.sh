2972e105dbc5ec993432ff0227de0f9e9154f9aa
Sven Anderson <sven@anderson.de>
2022-10-12 22:06:12 +0200

    tests: add CI job for tests on real hardware
    Signed-off-by: Sven Anderson <sven@anderson.de>

---
+++ tools/mk-ci-installer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -ex
+#
+# patch the installer image with a grub config for the CI
+#
+
+INSTALLER=$1
+mkdir -p obj
+CI_INSTALLER=${PWD}/obj/ci-installer.img
+
+CI_INSTALLER_GRUB_CFG=${PWD}/src/installer/grub-ci-installer.cfg
+KEY_DIR=${PWD}/keys/test-key
+SIGN=${PWD}/tools/gpg-sign-detached.sh
+
+TMPDIR=$(mktemp -d)
+cd ${TMPDIR}
+cpio -ivd -F ${INSTALLER}
+cp ${CI_INSTALLER_GRUB_CFG} boot/grub/grub.cfg
+rm boot/grub/grub.cfg.sig
+${SIGN} ${KEY_DIR} ${TMPDIR}/boot/grub/grub.cfg
+find . -type f | cpio -v -o -H newc -F ${CI_INSTALLER}
+rm -rf ${TMPDIR}
