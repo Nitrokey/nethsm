efae4c1d1bbd1bdc865f4d3d1931660eb45bec2f
Nils Ponsard | Nitrokey <nils@nitrokey.com>
2023-09-29 16:06:20 +0200

    feat: propagate exit code

--- tools/openapi-fuzzing.sh
+++ tools/openapi-fuzzing.sh
@@ -6,11 +6,16 @@ cd src/tests
 TEST_COUNT=500000
 
 
+failed=0
+
 # No auth
 ../keyfender/_build/default/test/test_server.exe 2> /dev/null &
 sleep 2
 NETHSM_URL="http://localhost:8080/api" ./provision_test.sh
 openapi-fuzzer run -i 405 --max-test-case-count $TEST_COUNT --spec ../../api.json --url http://localhost:8080/api/v1
+if [ $? -ne 0 ]; then
+    failed=1
+fi
 pkill test_server.exe || true
 
 # Operator
@@ -18,6 +23,9 @@ pkill test_server.exe || true
 sleep 2
 NETHSM_URL="http://localhost:8080/api" ./provision_test.sh
 openapi-fuzzer run -i 405 --max-test-case-count $TEST_COUNT --spec ../../api.json --url http://localhost:8080/api/v1   -H 'Authorization: Basic b3BlcmF0b3I6T3BlcmF0b3JPcGVyYXRvcg=='
+if [ $? -ne 0 ]; then
+    failed=1
+fi
 pkill test_server.exe || true
 
 # Administrator
@@ -25,4 +33,9 @@ pkill test_server.exe || true
 sleep 2
 NETHSM_URL="http://localhost:8080/api" ./provision_test.sh
 openapi-fuzzer run -i 405 --max-test-case-count $TEST_COUNT --spec ../../api.json --url http://localhost:8080/api/v1 -H 'Authorization: Basic YWRtaW46QWRtaW5pc3RyYXRvcg=='
+if [ $? -ne 0 ]; then
+    failed=1
+fi
 pkill test_server.exe || true
+
+exit $failed
