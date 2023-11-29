6ac46e950914e618e046b5b8d3b9dfc60af69fa8
Sven Anderson <sven@anderson.de>
2023-11-29 15:13:28 +0100

    platform: update known firmware versions
    Signed-off-by: Sven Anderson <sven@anderson.de>

--- src/u-root/uinit/firmwareversion.go
+++ src/u-root/uinit/firmwareversion.go
@@ -6,7 +6,9 @@
 package main
 
 var firmwareVersions = map[string]string{
-	"2eeb3fbb4a1e4533ab7246d05049a7676d4a378d62426976c32774263d945806": "1.0-devel",
+	"2eeb3fbb4a1e4533ab7246d05049a7676d4a378d62426976c32774263d945806": "0.9-devel",
+	"db89554134cbbc1f54e625a5df8e175a1f5189a3bfdffdfe249640a99a4bbbee": "1.0-devel",
+	"6164cc90d15caca5da0bbcba579c438b1e274dfb979b8aeeb5a497b2e4ab2e69": "1.0-prod",
 }
 
 const firmwarePCRIdx = 2
