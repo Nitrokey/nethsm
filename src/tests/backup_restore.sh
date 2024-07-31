a181214ae365e8ea98b34a72cf960f344d553e28
Sven Anderson <sven@anderson.de>
2024-07-31 02:33:25 +0200

    CI: use provisioned restore for HIL test

--- src/tests/backup_restore.sh
+++ src/tests/backup_restore.sh
@@ -8,30 +8,30 @@ if [[ "$STATE" != *Operational* ]] ; then
   exit 1
 fi
 
-echo "doing reboot"
-POST_admin /v1/system/reboot
+# echo "doing reboot"
+# POST_admin /v1/system/reboot
 
-echo "waiting for NetHSM"
-x=0
-while ! curl -s -k -f https://${NETHSM_IP}/api/v1/health/state ; do
-  printf "."
-  ((x++>10)) && echo "time out!" && exit 1
-  sleep 5
-done
-echo
+# echo "waiting for NetHSM"
+# x=0
+# while ! curl -s -k -f https://${NETHSM_IP}/api/v1/health/state ; do
+#   printf "."
+#   ((x++>10)) && echo "time out!" && exit 1
+#   sleep 5
+# done
+# echo
 
-STATE=$(GET /v1/health/state)
-if [[ "$STATE" != *Locked* ]] ; then
-  echo "State $STATE != Locked"
-  exit 1
-fi
+# STATE=$(GET /v1/health/state)
+# if [[ "$STATE" != *Locked* ]] ; then
+#   echo "State $STATE != Locked"
+#   exit 1
+# fi
 
-echo "unlocking"
-POST /v1/unlock <<EOM
-{
-  "passphrase": "UnlockPassphrase"
-}
-EOM
+# echo "unlocking"
+# POST /v1/unlock <<EOM
+# {
+#   "passphrase": "UnlockPassphrase"
+# }
+# EOM
 
 echo "creating backup"
 POST /v1/system/backup --user backup:BackupBackup -o /tmp/backup.bin
@@ -54,8 +54,19 @@ if [[ "$STATE" != *Unprovisioned* ]] ; then
   exit 1
 fi
 
+# provision hsm differently
+echo "Provisioning again differently."
+SYSTEM_TIME="$(date -u +%FT%TZ)"
+POST /v1/provision <<EOM
+{
+  "unlockPassphrase": "UnlockPassphrase2",
+  "adminPassphrase": "Administrator2",
+  "systemTime": "${SYSTEM_TIME}"
+}
+EOM
+
 echo "restoring backup"
-${CURL} -X POST -F arguments='{"backupPassphrase": "backupPassphrase"}' -F backup=@/tmp/backup.bin \
+${CURL} -X POST --user admin:Administrator2 -F arguments='{"backupPassphrase": "backupPassphrase"}' -F backup=@/tmp/backup.bin \
   https://${NETHSM_IP}/api/v1/system/restore || exit 1
 
 
@@ -72,6 +83,17 @@ POST /v1/unlock <<EOM
 }
 EOM
 
+STATE=$(GET /v1/health/state)
+if [[ "$STATE" != *Operational* ]] ; then
+  echo "State $STATE != Operational"
+  exit 1
+fi
+
+echo "restoring backup again"
+${CURL} -X POST --user admin:Administrator -F arguments='{"backupPassphrase": "backupPassphrase"}' -F backup=@/tmp/backup.bin \
+  https://${NETHSM_IP}/api/v1/system/restore || exit 1
+
+# should be directly operational
 STATE=$(GET /v1/health/state)
 if [[ "$STATE" != *Operational* ]] ; then
   echo "State $STATE != Operational"
