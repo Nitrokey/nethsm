0f20b6d08cad63795ef4108ea6f9f38472ec9d98
Sven Anderson <sven@anderson.de>
2024-09-07 23:34:23 +0200

    S-Net: use netfilter bridge rules for MAC NAT

--- src/u-root/uinit/go.mod
+++ src/u-root/uinit/go.mod
@@ -8,14 +8,20 @@ go 1.21
 toolchain go1.22.3
 
 require (
-	github.com/canonical/go-tpm2 v1.7.5
+	github.com/canonical/go-tpm2 v1.7.6
+	github.com/google/nftables v0.2.0
 	github.com/u-root/u-root v0.14.0
 )
 
 require (
 	github.com/canonical/go-sp800.108-kdf v0.0.0-20210315104021-ead800bbf9a0 // indirect
-	golang.org/x/crypto v0.25.0 // indirect
-	golang.org/x/net v0.27.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/josharian/native v1.1.0 // indirect
+	github.com/mdlayher/netlink v1.7.2 // indirect
+	github.com/mdlayher/socket v0.5.1 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637 // indirect
 )
--- src/u-root/uinit/s_net_external.go
+++ src/u-root/uinit/s_net_external.go
@@ -5,8 +5,19 @@ package main
 
 import (
 	"log"
-	"path/filepath"
+	"net"
+
+	"github.com/google/nftables"
+	"github.com/google/nftables/expr"
 )
+
+const (
+	extIface     = "eth0"
+	intIface     = "net0"
+	keyfenderMAC = "b2:e0:5a:97:07:c2"
+)
+
+var broadcastMAC = net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
 
 // sNetExternalActions are executed for S-Net-External.
 func sNetExternalActions() {
@@ -21,14 +32,13 @@ func sNetExternalActions() {
 	// child interface attached to the bridge, so intentionally start with
 	// eth* here.
 	G.s.Execf("/bbin/ip link add br0 type bridge")
-	netPaths := G.s.Glob("/sys/class/net/eth*")
-	netPaths = append(netPaths, G.s.Glob("/sys/class/net/net*")...)
-	for _, netPath := range netPaths {
-		_, netIf := filepath.Split(netPath)
-		G.s.Execf("/bbin/ip link set %s master br0", netIf)
-		G.s.Execf("/bbin/ip link set dev %s up", netIf)
-	}
+	G.s.Execf("/bbin/ip link set %s master br0", extIface)
+	G.s.Execf("/bbin/ip link set dev %s up", extIface)
+	G.s.Execf("/bbin/ip link set %s master br0", intIface)
+	G.s.Execf("/bbin/ip link set dev %s up", intIface)
 	G.s.Execf("/bbin/ip link set dev br0 up")
+
+	setupNFTables()
 
 	dumpNetworkStatus()
 
@@ -36,4 +46,221 @@ func sNetExternalActions() {
 		log.Printf("Script failed: %v", err)
 		return
 	}
+}
+
+func getMACAddress(interfaceName string) (net.HardwareAddr, error) {
+	iface, err := net.InterfaceByName(interfaceName)
+	if err != nil {
+		return nil, err
+	}
+	return iface.HardwareAddr, nil
+}
+
+func setupNFTables() {
+	c, err := nftables.New()
+	if err != nil {
+		log.Fatalf("Failed to create nftables connection: %v", err)
+	}
+
+	// Get the real MAC address of iface
+	extMAC, err := getMACAddress(extIface)
+	if err != nil {
+		log.Fatalf("Failed to get MAC address of %s: %v", extIface, err)
+	}
+
+	// Parse destination MAC address
+	intMAC, err := net.ParseMAC(keyfenderMAC)
+	if err != nil {
+		log.Fatalf("Failed to parse destination MAC: %v", err)
+	}
+
+	// Create the "nat" table in the bridge family if it doesn't exist
+	nat := c.AddTable(&nftables.Table{
+		Family: nftables.TableFamilyBridge,
+		Name:   "nat",
+	})
+
+	// Create PREROUTING and POSTROUTING chains
+	prerouting := c.AddChain(&nftables.Chain{
+		Name:     "PREFORWARD",
+		Table:    nat,
+		Type:     nftables.ChainTypeFilter,
+		Hooknum:  nftables.ChainHookPrerouting,
+		Priority: nftables.ChainPriorityRef(-300),
+	})
+
+	postrouting := c.AddChain(&nftables.Chain{
+		Name:     "POSTFORWARD",
+		Table:    nat,
+		Type:     nftables.ChainTypeFilter,
+		Hooknum:  nftables.ChainHookPostrouting,
+		Priority: nftables.ChainPriorityRef(300),
+	})
+
+	// PREROUTING rule
+	c.AddRule(&nftables.Rule{
+		Table: nat,
+		Chain: prerouting,
+		Exprs: exp{}.iifname(extIface).arpHWTarget(extMAC).arpSetHWTarget(intMAC).cont(),
+	})
+	c.AddRule(&nftables.Rule{
+		Table: nat,
+		Chain: prerouting,
+		Exprs: exp{}.iifname(extIface).etherDst(extMAC).etherSetDst(intMAC).accept(),
+	})
+	c.AddRule(&nftables.Rule{
+		Table: nat,
+		Chain: prerouting,
+		Exprs: exp{}.iifname(extIface).etherDst(broadcastMAC).accept(),
+	})
+	c.AddRule(&nftables.Rule{
+		Table: nat,
+		Chain: prerouting,
+		Exprs: exp{}.iifname(extIface).drop(),
+	})
+
+	// POSTROUTING rule
+	c.AddRule(&nftables.Rule{
+		Table: nat,
+		Chain: postrouting,
+		Exprs: exp{}.oifname(extIface).arpHWSender(intMAC).arpSetHWSender(extMAC).cont(),
+	})
+	c.AddRule(&nftables.Rule{
+		Table: nat,
+		Chain: postrouting,
+		Exprs: exp{}.oifname(extIface).etherSrc(intMAC).etherSetSrc(extMAC).accept(),
+	})
+
+	// Commit the changes
+	if err := c.Flush(); err != nil {
+		log.Fatalf("Failed to commit changes: %v", err)
+	}
+
+	log.Printf("nftables rules for %s/%s added successfully", extMAC, intMAC)
+}
+
+const (
+	offsetEthDst      = 0
+	offsetEthSrc      = 6
+	offsetEthType     = 12
+	offsetArpHWSender = 8
+	offsetArpHWTarget = 18
+	arpHeader         = "\x00\x01\x08\x00\x06\x04"
+	ethTypeArp        = "\x08\x06"
+)
+
+type exp []expr.Any
+
+func (e exp) iifname(iface string) exp {
+	return ifname(e, expr.MetaKeyIIFNAME, iface)
+}
+
+func (e exp) oifname(iface string) exp {
+	return ifname(e, expr.MetaKeyOIFNAME, iface)
+}
+
+func (e exp) etherDst(mac net.HardwareAddr) exp {
+	return match(e, expr.PayloadBaseLLHeader, offsetEthDst, mac)
+}
+
+func (e exp) etherSrc(mac net.HardwareAddr) exp {
+	return match(e, expr.PayloadBaseLLHeader, offsetEthSrc, mac)
+}
+
+func (e exp) etherSetDst(mac net.HardwareAddr) exp {
+	return write(e, expr.PayloadBaseLLHeader, offsetEthDst, mac)
+}
+
+func (e exp) etherSetSrc(mac net.HardwareAddr) exp {
+	return write(e, expr.PayloadBaseLLHeader, offsetEthSrc, mac)
+}
+
+func (e exp) arpHWSender(mac net.HardwareAddr) exp {
+	return arpHW(e, offsetArpHWSender, mac)
+}
+
+func (e exp) arpHWTarget(mac net.HardwareAddr) exp {
+	return arpHW(e, offsetArpHWTarget, mac)
+}
+
+func (e exp) arpSetHWSender(mac net.HardwareAddr) exp {
+	return write(e, expr.PayloadBaseNetworkHeader, offsetArpHWSender, mac)
+}
+
+func (e exp) arpSetHWTarget(mac net.HardwareAddr) exp {
+	return write(e, expr.PayloadBaseNetworkHeader, offsetArpHWTarget, mac)
+}
+
+func ifname(e exp, k expr.MetaKey, iface string) exp {
+	e = append(e,
+		&expr.Meta{Key: k, Register: 1})
+	e = append(e,
+		&expr.Cmp{
+			Op:       expr.CmpOpEq,
+			Register: 1,
+			Data:     []byte(iface + "\x00"),
+		})
+	return e
+}
+
+func arpHW(e exp, offset uint32, mac net.HardwareAddr) exp {
+	e = match(e, expr.PayloadBaseLLHeader, offsetEthType, []byte(ethTypeArp))
+	e = match(e, expr.PayloadBaseNetworkHeader, 0, []byte(arpHeader))
+	e = match(e, expr.PayloadBaseNetworkHeader, offset, mac)
+	return e
+}
+
+func match(e exp, base expr.PayloadBase, offset uint32, data []byte) exp {
+	// Load
+	e = append(e,
+		&expr.Payload{
+			DestRegister: 1,
+			Base:         base,
+			Offset:       offset,
+			Len:          uint32(len(data)),
+		})
+	// Match source MAC
+	e = append(e,
+		&expr.Cmp{
+			Op:       expr.CmpOpEq,
+			Register: 1,
+			Data:     data,
+		})
+	return e
+}
+
+func write(e exp, base expr.PayloadBase, offset uint32, data []byte) exp {
+	// Set new source MAC
+	e = append(e,
+		&expr.Immediate{
+			Register: 1,
+			Data:     data,
+		})
+	e = append(e,
+		&expr.Payload{
+			OperationType:  expr.PayloadWrite,
+			SourceRegister: 1,
+			Base:           base,
+			Offset:         offset,
+			Len:            uint32(len(data)),
+		})
+	return e
+}
+
+func (e exp) accept() exp {
+	e = append(e,
+		&expr.Verdict{Kind: expr.VerdictAccept})
+	return e
+}
+
+func (e exp) cont() exp {
+	e = append(e,
+		&expr.Verdict{Kind: expr.VerdictContinue})
+	return e
+}
+
+func (e exp) drop() exp {
+	e = append(e,
+		&expr.Verdict{Kind: expr.VerdictDrop})
+	return e
 }
