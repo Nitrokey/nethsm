807d7b9ed8e88532a1352bae96f08b15e6a159c4
Sven Anderson <sven@anderson.de>
2025-11-26 04:15:48 +0100

    tests: fix and improve perftest.go

--- tools/test-on-hw.sh
+++ tools/test-on-hw.sh
@@ -17,7 +17,7 @@ curl -s -k -X PUT -H "content-type: application/json" -d \
     '{"ipAddress":"0.0.0.0","port":0,"logLevel":"warning"}' \
     https://admin:Administrator@${NETHSM_IP}/api/v1/config/logging
 
-flock /tmp/perftest.lock go run ./perftest.go -host ${NETHSM_IP}:443 -j 10 \
+flock /tmp/perftest.lock go run ./perftest.go -host ${NETHSM_IP}:443 -j 50 \
     p256 p384 p521 rsa1024 rsa2048 rsa3072 rsa4096 ed25519 p256k1 \
     p256k1-bip340 brainpoolp256 brainpoolp384 brainpoolp512 aes-cbc rnd-1024 \
     p256-gen rsa2048-gen rsa3072-gen rsa4096-gen
