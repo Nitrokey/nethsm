22eefc243f9bf8cf863dfd2f242aaec06f4698dc
Virgile Robles <virgile.robles@protonmail.ch>
2026-03-23 18:13:45 +0100

    migration: avoid delete+set in one txn

--- src/keyfender/config_store.ml
+++ src/keyfender/config_store.ml
@@ -552,25 +552,26 @@ module Make (KV : Kv_ext.Platform) = struct
     let old = { t with device_id = ""; migration_in_progress = true } in
     let deferred_nb = ref 0 in
     batch t (fun t ->
-        let set_or_delay t k data =
+        let set_or_remove_and_delay ~remove t k data =
           let src = key_path old k in
           let dst = key_path t k in
           Logs.info (fun f ->
               f "migrating %a to %a" Mirage_kv.Key.pp src Mirage_kv.Key.pp dst);
           set t k data >>= function
           | Error `Restore_in_progress -> Lwt_result.return ()
-          | Error `Missing_domain_key ->
+          | Error `Missing_domain_key -> (
               Logs.info (fun f ->
                   f "need domain key to migrate %a! deferring to after unlock"
                     Mirage_kv.Key.pp src);
-              incr deferred_nb;
-              append_pending_unlock_migrations t k data
+              remove () >>= function
+              | Ok () ->
+                  incr deferred_nb;
+                  append_pending_unlock_migrations t k data
+              | Error e -> Lwt.return (Error e))
           | x -> Lwt.return x
         in
         let move k data =
-          remove old k >>= function
-          | Ok () -> set_or_delay t k data
-          | Error e -> Lwt.return (Error e)
+          set_or_remove_and_delay ~remove:(fun () -> remove old k) t k data
         in
         let migrate_generic (type a) (k : a k) =
           get_opt old k >>= function
@@ -643,6 +644,6 @@ module Make (KV : Kv_ext.Platform) = struct
           migrate_generic Unattended_boot >>= fun () ->
           migrate_log_config () >>= fun () ->
           migrate_ip_config () >>= fun () ->
-          set_or_delay t Version Version.V1 >|= fun () -> !deferred_nb
+          move Version Version.V1 >|= fun () -> !deferred_nb
         else Lwt_result.return !deferred_nb)
 end
