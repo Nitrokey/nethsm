dd794cb60b9d96779be7283ec52af5481d288296
Edwin Török <edwin@tarides.com>
2026-05-11 11:26:18 +0100

    tests: use at_exit instead of Fun.protect due to alcotest bug
    This is fixed here https://github.com/mirage/alcotest/pull/439,
    but using `at_exit` should be more reliable and works with older
    alcotest too.
    
    Signed-off-by: Edwin Török <edwin@tarides.com>

--- src/tests/run_generated_tests.ml
+++ src/tests/run_generated_tests.ml
@@ -326,10 +326,10 @@ let tests backend = api_tests backend @ custom_tests backend
 
 let main (module B : BACKEND) =
   let ctx = B.init () in
+  at_exit (fun () -> B.finish ctx);
   try
     Sys.catch_break true;
-    Fun.protect ~finally:(fun () -> B.finish ctx) @@ fun () ->
-    Alcotest.run ~and_exit:false ~argv:Sys.argv "unikernel" (tests (module B))
+    Alcotest.run ~argv:Sys.argv "unikernel" (tests (module B))
   with Alcotest.Test_error -> exit 1
 
 let () = main (module KeyfenderUnikernel)
